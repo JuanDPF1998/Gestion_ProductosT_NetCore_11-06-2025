35f6576e59abd71085782412a7a7cd0fe0231ccf
JuanDPF1998 <JuanDanielPF1998@gmail.com>
2025-06-11 12:28:40 -0600

    creacion del controlador productos

---
+++ Controllers/productoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gestion_ProductosT.Controllers
+{
+    public class productoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
