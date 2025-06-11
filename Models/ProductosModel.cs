00e8fcf14bd5ba5e0a084e166bdb47761679ebe8
JuanDPF1998 <JuanDanielPF1998@gmail.com>
2025-06-11 10:36:26 -0600

    Entidad o clase modelo programada

---
+++ Models/ProductosModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gestion_ProductosT.Models
+{
+    public class ProductosModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public string Stock { get; set; } = string.Empty;
+    }
+}
