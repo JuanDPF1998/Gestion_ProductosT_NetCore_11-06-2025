db2b79e433b1ef3e909a6481653a76a4fed3522b
JuanDPF1998 <JuanDanielPF1998@gmail.com>
2025-06-11 12:16:05 -0600

    Cuarto cambio configurar el servicio de conexion program

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 
 
+using Gestion_ProductosT.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseAzureSql(builder.Configuration.GetConnectionString("conn")));
 
 var app = builder.Build();
 
