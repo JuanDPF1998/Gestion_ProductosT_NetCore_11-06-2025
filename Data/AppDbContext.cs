ed77351db31742400ae11421a5c10c33eb4aeccb
JuanDPF1998 <JuanDanielPF1998@gmail.com>
2025-06-11 11:35:45 -0600

    Programacion de la clase AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,11 @@
-﻿namespace Gestion_ProductosT.Data
+﻿using Gestion_ProductosT.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestion_ProductosT.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<ProductosModel> productos { get; set; }
     }
 }
