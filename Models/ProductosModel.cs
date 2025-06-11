b6f45f2e9431c82f96d8046f141314325d6a8bd6
JuanDPF1998 <JuanDanielPF1998@gmail.com>
2025-06-11 11:37:28 -0600

    se agrega el mapeo a la base de datos en entidad

--- Models/ProductosModel.cs
+++ Models/ProductosModel.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Gestion_ProductosT.Models
 {
+    [Table("productos")]
     public class ProductosModel
     {
         [Key]
