921d42f2a344bcb615f9128e589e552890cabcee
JuanDPF1998 <JuanDanielPF1998@gmail.com>
2025-06-11 12:51:32 -0600

    Programacion del primer metodo listar

--- Controllers/productoController.cs
+++ Controllers/productoController.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Gestion_ProductosT.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gestion_ProductosT.Controllers
 {
     public class productoController : Controller
     {
+        public readonly AppDbContext _context;
+
+        public productoController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            try
+            {
+                var producto = _context.productos.Find();
+                return View(producto);
+            }
+            catch (DbUpdateException ex)
+            {
+                return Content($"Error de conexion de base de datos {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                return Content("Excepcion no controlada :" + ex.Message);
+            }
             return View();
         }
     }
