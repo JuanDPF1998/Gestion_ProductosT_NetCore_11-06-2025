393650c3c96c6f2e8e36a1132a653f6b24b62674
JuanDPF1998 <JuanDanielPF1998@gmail.com>
2025-06-11 09:27:13 -0600

    primer commit Gestion_ProtudctosT

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gestion_ProductosT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
